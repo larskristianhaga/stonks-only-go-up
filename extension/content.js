be76e6a3c34e2bf90b5f6fcc9ff8357ebfeb4a2b
Lars Kristian Haga <LarsKHaga@gmail.com>
2024-09-24 22:40:03 +0200

    Minor tweaks to also remove the button from more pages

--- extension/content.js
+++ extension/content.js
@@ -1,36 +1,40 @@
 // Function to replace the sell button
 function replaceSellFundButton() {
 
-  const element = document.querySelector('[data-testid="sell-fund-button"]');
+    const sellLink = document.querySelector('a[href*="sell"]');
 
-  if (element) {
-    const span = document.createElement('span');
-    span.textContent = '💎👐';
-    element.replaceWith(span);
-  } else {
-    console.log("Button was not found.");
-  }
+    if (sellLink) {
+        const span = document.createElement('span');
+        span.textContent = '💎👐';
+        span.style.cursor = 'not-allowed';
+
+        // Add something to on hover
+        span.setAttribute('title', 'We do not support paper hands people. 💎👐');
+
+        sellLink.replaceWith(span);
+    } else {
+        console.log("Sell link was not found.");
+    }
 }
 
 // Function to observe DOM changes
 function observeDOMChanges() {
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.addedNodes.length) {
-        replaceSellFundButton();
-      }
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.addedNodes.length) {
+                replaceSellFundButton();
+            }
+        });
     });
-  });
 
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-  });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
 }
 
 // Run the function when the window is fully loaded
 window.addEventListener('load', () => {
-  // console.log("Window fully loaded.");
-  replaceSellFundButton();
-  observeDOMChanges();
+    replaceSellFundButton();
+    observeDOMChanges();
 });
